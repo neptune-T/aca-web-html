fcb7f9b3147d3657f8489e787314477f7f6f83a3
plote <2980976495@qq.com>
2025-06-17 11:38:01 +0800

    rename

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,8 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
   // Set basePath and assetPrefix only for production builds
-  basePath: isProd ? '/plote-homepage' : '',
-  assetPrefix: isProd ? '/plote-homepage/' : '',
+  basePath: isProd ? '/aca-web-html' : '',
+  assetPrefix: isProd ? '/aca-web-html/' : '',
   images: {
     unoptimized: true,
   },
