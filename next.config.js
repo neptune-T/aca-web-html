d08d1a22e8072d0a1a2ba1d24129260ab34d8a28
Plote <2980976495@qq.com>
2025-09-29 23:40:52 +0800

    pdf

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: '/aca-web-html',
-  assetPrefix: '/aca-web-html/',
-  output: 'export', // 如果是静态导出，确保启用
-  images: { unoptimized: true }, // 新增：禁用图像优化，修复导出错误
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   basePath: '/aca-web-html',
+//   assetPrefix: '/aca-web-html/',
+//   output: 'export', // 如果是静态导出，确保启用
+//   images: { unoptimized: true }, // 新增：禁用图像优化，修复导出错误
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
